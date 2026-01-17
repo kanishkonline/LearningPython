87de1e3dfcb2956c2208c0b25127a7382e4c1a67
kanishkonline <kanishkonline.works@gmail.com>
2026-01-17 12:19:09 +0530

    Newly Updated

--- PYTHON ASSIGNMENTS BY TUTEDUDE/Assignment 1/Task1.py
+++ PYTHON ASSIGNMENTS BY TUTEDUDE/Assignment 1/Task1.py
@@ -24,20 +24,3 @@ print("Multiplication: ", multi)
 print("Division: ", div)
 
 print("FINAL RESULTS : ",add, sub, multi, div)
-
-
-
-#Task 2: Create a Personalized Greeting
-
-# Problem Statement: Write a Python program that:
-# 1.  Takes a user's first name and last name as input.
-# 2.  Concatenates the first name and last name into a full name.
-# 3.  Prints a personalized greeting message using the full name.
-
-
-First_Name = input("Enter Your First Name: ")
-Second_Name = input("Enter Your Second Name: ")
-
-Full_Name = First_Name + " " + Second_Name
-
-print("Hello,",Full_Name,"Welcome to the world of coding")
--- README.md
+++ README.md
@@ -1,2 +1,20 @@
-# LearningPython
-kanishk singh
+# LearningPython REPOSITORIE
+
+## Python Assignments – Tutedude
+
+This repository contains Python assignments completed as part of the Tutedude Python course.
+
+### Folders Structure
+- Assignment 1/
+  - Task1.py – Basic mathematical operations
+  - Task2.py – Personalized greeting
+
+- Assignment 2/
+  - Contains programs for Assignment 2
+
+### How to Run
+Navigate to the assignment folder and
+
+
+## Author
+Kanishk Singh
